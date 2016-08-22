62204f824c6734f5ab7acef62b7a055bbe626b48
Peter Beshai <peter.beshai@gmail.com>
2016-08-21 21:58:53 -0400

    Remove old test

--- test/interactiveScale-test.js
+++ test/interactiveScale-test.js
@@ -1,7 +1,6 @@
 var tape = require('tape'),
     scaleInteractive = require('../');
 
-tape('scaleInteractive() returns the answer to the ultimate question of life, the universe, and everything.', function (test) {
-  test.equal(scaleInteractive.scaleInteractive(), 42);
-  test.end();
-});
+// tape('scaleInteractive() disappoints everyone by not having tests.', function (test) {
+//   test.end();
+// });
